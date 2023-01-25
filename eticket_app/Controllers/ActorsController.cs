f1e4f859f19059b790448f590598d49d36b01c99
Arsenii <senyazp80@gmail.com>
2023-01-25 20:46:35 +0100

    new Actor controller added

--- eticket_app/Controllers/ActorsController.cs
+++ eticket_app/Controllers/ActorsController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace eticket_app.Controllers
 {
     public class ActorsController : Controller
     {
+        private readonly AppDbContext _context;
+        public ActorsController(AppDbContext context)
+        {
+            _context= context;
+        }
         public IActionResult Index()
         {
+            var data = _context.Actors.ToList();
             return View();
         }
     }
