02e462bd50c5f97c9c4ee4c8a56f70a314039fb7
Arsenii <senyazp80@gmail.com>
2023-01-25 21:19:52 +0100

    New controllers added

---
+++ eticket_app/Controllers/CinemasController.cs
@@ -0,0 +1,20 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace eticket_app.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CinemasController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Producers.ToList();
+            return View();
+        }
+    }
+}
---
+++ eticket_app/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace eticket_app.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly AppDbContext _context;
+        public MoviesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Movies.ToList();
+            return View();
+        }
+    }
+}
